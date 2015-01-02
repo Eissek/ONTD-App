29a2868db82cedddeb03a71f999467d2ce77ab64
Eissek <sane.gez@gmail.com>
2015-01-02 22:51:49 +0000

    added get first tweet or image functionality

--- js/controllers/preview.js
+++ js/controllers/preview.js
@@ -9,7 +9,7 @@ ontd.controllers = ontd.controllers || {};
 
 (function () {
 
-
+    
     ontd.controllers.getFirstImage = function (data) {
 	var element = [];
 	var temp = document.createElement("div");
@@ -17,6 +17,42 @@ ontd.controllers = ontd.controllers || {};
 	element = temp.getElementsByTagName("img");
 	
 	return element;
+    };
+
+    ontd.controllers.getFirstTweet = function (data) {
+	var element = [];
+	var temp = document.createElement("div");
+	temp.innerHTML = data;
+	element = temp.getElementsByClassName("twitter-tweet");
+	// element = element.getElementsByClassName()
+	return element;
+
+    };
+
+    // Helps to display either an image or embedded tweet
+    ontd.controllers.getFirstEmbed = function (data) {
+	var temp = document.createElement("div");
+	temp.innerHTML = data;
+	var element = [];
+	// var nodes = temp.childNodes;
+	var nodes = temp.children;
+	console.log("THESE ARE THE NODES " + nodes);
+	console.log(nodes[0].tagName);
+	for (var i = 0; i < nodes.length; i++) {
+	    if (nodes[i].tagName === "IMG") {
+		console.log("Found image");
+		console.log(nodes[i]);
+		element = nodes[i];
+		break;
+	    } else if (nodes[i].tagName === "BLOCKQUOTE") {
+		console.log("Found Quote");
+		element = nodes[i];
+		break;
+	    }
+	}
+
+	return element;
+
     };
     
     // Removes the content text that is not visible
@@ -33,36 +69,32 @@ ontd.controllers = ontd.controllers || {};
 	// var x =  $(content).text();
 
 	// First image found is added to the preview
-	var images = ontd.controllers.getFirstImage(content);
+	// var images = ontd.controllers.getFirstImage(content);
+	var images = ontd.controllers.getFirstEmbed(content);
 	var newText = "";
-	if (images.length > 0) {
-	    console.log("FOUND IMAGES");
-	    // console.log(images[0].outerHTML);
-	    // images = images[0].outerHTML;
-	    var text = content.replace(/<(?:.|\n)*?>/gm, '');
-	    // console.log("Word count is " + text.split(/\s+/).length);
-	    if (text.split(/\s+/).length > 40) {
-		text = text.split(" ").splice(0, 40).join(" ");
-		console.log("spliced EVERYTHING");
-	    }
-	    
-	    text += "..."; // Used to indicate more text to be read
-;
-	    newText = images[0].outerHTML + "<br/><br/>"+ text;
-	    // console.log(newText);
+	var text = content.replace(/<(?:.|\n)*?>/gm, '');
+	
+	
+	if (text.split(/\s+/).length > 40) {
+	    text = text.split(" ").splice(0, 40).join(" ");
+	    console.log("spliced EVERYTHING");
 	}
-	// console.log(images[0]);
-
-	// return images[0].outerHTML;
-	// }
+	
+	text += "..."; // Used to indicate more text to be read
+	// Image is added before the text for formatting purposes
+	// if (images.length > 0)
+	if (images !== "undefined")
+	    newText = images.outerHTML + "<br/> <br/>" + text;
+	
 	return newText;
 	
     };
     // Resizes image so it's visible in element window
     ontd.controllers.previewImage = function (image) {
-
+	
     };
-    
+
+
     ontd.controllers.checkWindowHeight = function() {
 	var check = true;
 	if (window.innerHeight >= 540) {
